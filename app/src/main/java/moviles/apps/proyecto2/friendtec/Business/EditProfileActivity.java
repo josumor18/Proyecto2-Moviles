de9bf8de3c013419f2deae58808143578a86fb86
Alvaro <visvaro@gmail.com>
2018-06-07 01:17:08 -0600

    Signup Frontend

--- app/src/main/java/moviles/apps/proyecto2/friendtec/Business/EditProfileActivity.java
+++ app/src/main/java/moviles/apps/proyecto2/friendtec/Business/EditProfileActivity.java
@@ -2,14 +2,91 @@ package moviles.apps.proyecto2.friendtec.Business;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import moviles.apps.proyecto2.friendtec.R;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    EditText edt_correo;
+    EditText edt_nombre;
+    EditText edt_contasenaActual;
+    EditText edt_contrasenaNueva;
+    EditText edt_contrasenaConf;
+
+    Button btn_guardar;
+    Button btn_cambiarCon;
+
+    Usuario_Singleton user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        user = Usuario_Singleton.getInstance();
+
+        edt_correo = findViewById(R.id.edt_correo);
+        edt_nombre = findViewById(R.id.edt_nombre);
+        edt_contrasenaNueva = findViewById(R.id.edt_nuevaCon);
+        edt_contasenaActual = findViewById(R.id.edt_contrasena);
+        edt_contrasenaConf = findViewById(R.id.edt_nuevaConfCon);
+
+        btn_guardar = findViewById(R.id.btn_guardar);
+        btn_cambiarCon = findViewById(R.id.btn_cambiarCon);
+
+        edt_nombre.setText(user.getNombre());
+        edt_correo.setText(user.getEmail());
     }
+
+    public void guardarCambiosClicked(View view){
+
+        if (!TextUtils.isEmpty(edt_correo.getText()) && !TextUtils.isEmpty(edt_nombre.getText())) {
+
+            /*ExecuteChange executeChange = new ExecuteChange(user.getId(),
+                    edt_nombre.getText().toString(), edt_correo.getText().toString(),user.getAuth_token());
+
+            executeChange.execute();*/
+        }
+        else {
+            edt_nombre.setError("Campo Requerido");
+            edt_correo.setError("Campo Requerido");
+        }
+
+
+
+
+    }
+    public void cambiarContrasenaClicked(View view){
+        String actualCont = edt_contasenaActual.getText().toString();
+
+        String nuevaCont = edt_contrasenaNueva.getText().toString();
+        boolean val1 = true;
+        boolean val2 = true;
+
+        if (nuevaCont.length() < 6 || TextUtils.isEmpty(nuevaCont)) {
+            val1 = false;
+            edt_contrasenaNueva.setError("Min. 6 caracteres");
+            edt_contrasenaNueva.setText("");
+            edt_contrasenaConf.setText("");
+        }
+
+        if (!nuevaCont.equals(edt_contrasenaConf.getText().toString())) {
+            val2 = false;
+            edt_contrasenaConf.setError("Contraseñas no coinciden");
+
+        }
+
+        if (val1 && val2) {
+            /*ExecuteChange exChange = new ExecuteChange(user.getId(),edtNombre.getText().toString(),
+                    edtCorreo.getText().toString(),edtContrasena.getText().toString(),
+                    edtNuevaCon.getText().toString(),user.getAuth_token());
+
+            exChange.execute();*/
+        }
+    }
+
 }
--- app/src/main/java/moviles/apps/proyecto2/friendtec/Business/SignUpActivity.java
+++ app/src/main/java/moviles/apps/proyecto2/friendtec/Business/SignUpActivity.java
@@ -1,15 +1,176 @@
 package moviles.apps.proyecto2.friendtec.Business;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import moviles.apps.proyecto2.friendtec.Data.API_Access;
 import moviles.apps.proyecto2.friendtec.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText edt_carne;
+    EditText edt_correo;
+    EditText edt_nombre;
+    EditText edt_contrasena;
+    EditText edt_confcontrasena;
+    Button btn_registrar;
+    Spinner sp_carrera;
+
+    ArrayList<EditText> campos_obligatorios;
+
+    private static String[] listCarreras = {"Ing.Computacion","Ing.Electria","Administracion de Empresas", "Ing.Ambiental",
+        "Ing.Civil", "Ing.Computadores"};
+
+    String carreraSelected = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        edt_carne = findViewById(R.id.edt_carne);
+        edt_correo = findViewById(R.id.edt_email);
+        edt_nombre = findViewById(R.id.edt_nombre);
+        edt_contrasena = findViewById(R.id.edt_password);
+        edt_confcontrasena = findViewById(R.id.edt_confirm_password);
+        btn_registrar = findViewById(R.id.btn_register);
+        sp_carrera = findViewById(R.id.sp_carrera);
+
+        campos_obligatorios = new ArrayList<>();
+        campos_obligatorios.add(edt_carne);
+        campos_obligatorios.add(edt_correo);
+        campos_obligatorios.add(edt_contrasena);
+
+        ArrayAdapter adapterCarrera = new ArrayAdapter(this.getApplicationContext(),android.R.layout.simple_spinner_item,listCarreras);
+
+        adapterCarrera.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        sp_carrera.setAdapter(adapterCarrera);
+
+        sp_carrera.setSelection(0, true);
+        View vi = sp_carrera.getSelectedView();
+        ((TextView)vi).setTextColor(getResources().getColor(R.color.colorPrimary));
+
+        sp_carrera.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                //Change the selected item's text color
+                ((TextView) view).setTextColor(getResources().getColor(R.color.colorPrimary));
+                carreraSelected = listCarreras[sp_carrera.getSelectedItemPosition()];
+                Toast.makeText(getApplicationContext(), carreraSelected, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
+    }
+
+    public void registerClicked (View view){
+
+        //Validaciones de campos
+        String pass1 = edt_contrasena.getText().toString();
+        String pass2 = edt_confcontrasena.getText().toString();
+
+        for(EditText edit: campos_obligatorios){
+            if(TextUtils.isEmpty(edit.getText())){
+                edit.setError("Campo obligatorio");
+            }
+        }
+
+        if(pass1.length()<6){
+            //Toast.makeText(RegistrarActivity.this, "La contraseña debe contener minimo 6 caracteres", Toast.LENGTH_LONG).show();
+            edt_contrasena.setError("Min. 6 caracteres");
+            edt_contrasena.setText("");
+            edt_confcontrasena.setText("");
+        }
+        else if(!pass1.equals(pass2)){
+
+            //txtInvalidRegis.setVisibility(1);
+            Toast.makeText(this, "Contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            edt_contrasena.setText("");
+            edt_confcontrasena.setText("");
+
+        }
+        else{
+
+
+            ExecuteRegister register = new ExecuteRegister(edt_carne.getText().toString(),carreraSelected
+                    ,edt_correo.getText().toString(), edt_nombre.getText().toString()
+                    ,edt_contrasena.getText().toString());
+            register.execute();
+
+        }
+
+    }
+
+    public class ExecuteRegister extends AsyncTask<String,Void,String> {
+        private String name;
+        private String carrera;
+        private String carne;
+        private String email;
+        private String password;
+        private boolean isRegistered = false;
+
+        public ExecuteRegister(String carne, String carrera, String email,String name, String password){
+            this.carne = carne;
+            this.email = carrera;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+
+            API_Access api = API_Access.getInstance();
+            //isRegistered = api.register(carne,carrera,email,name,password);
+
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(isRegistered) {
+                Toast.makeText(SignUpActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            else {
+                /*String mensaje = "Fail";
+                try {
+                    mensaje = (API_Access.getInstance().getJsonObjectResponse()).getString("message");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }*/
+
+                Toast.makeText(SignUpActivity.this, "Registro fallido" , Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
+
+
     }
 }
