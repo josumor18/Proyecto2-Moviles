66c4fc1bb7afca8a86cb897b01d647f8010beb41
Alvaro <visvaro@gmail.com>
2018-06-08 01:43:55 -0600

    new post-friends layout

--- app/src/main/java/moviles/apps/proyecto2/friendtec/Business/ProfileActivity.java
+++ app/src/main/java/moviles/apps/proyecto2/friendtec/Business/ProfileActivity.java
@@ -3,9 +3,13 @@ package moviles.apps.proyecto2.friendtec.Business;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 import moviles.apps.proyecto2.friendtec.R;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ArrayList<Post> posts = new ArrayList<Post>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/moviles/apps/proyecto2/friendtec/Business/SearchFragment.java
+++ app/src/main/java/moviles/apps/proyecto2/friendtec/Business/SearchFragment.java
@@ -2,8 +2,6 @@ package moviles.apps.proyecto2.friendtec.Business;
 
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -189,7 +186,7 @@ public class SearchFragment extends Fragment {
                 view = inflater.inflate(R.layout.search_listview_item, null);
             }
 
-            ImageView imgUsuario = view.findViewById(R.id.imgSearchUser);
+            ImageView imgUsuario = view.findViewById(R.id.img_UserRequest);
             TextView txtUsername = view.findViewById(R.id.txtUsernamSearch);
             Button btnAddDelAmigo = view.findViewById(R.id.btnAddDelAmigo);
 
